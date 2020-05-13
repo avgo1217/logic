0674ac0492597eaae758462dc4ebf4a80a494733
Ryan Kim <1217rkim@gmail.com>
2020-05-13 16:59:56 -0500

    another change

--- app/views.py
+++ app/views.py
@@ -109,7 +109,7 @@ def login():
         login_user(user,remember=form.remember_me.data)
         user.update_scenarios_table(1,'all_scenarios.csv')
         user.add_videos_table()
-        all_scenarios_df = pd.read_sql(db.session.query(Scenarios).statement,db.session.bind)
+        
         #user.add_videos_table('temp_videos.csv')
 
         next_page = url_for('aim_tracker')
