7f2a89201ab971f41bffa81febb69be16ffed6d7
Ryan Kim <1217rkim@gmail.com>
2020-05-13 17:22:10 -0500

    mac added

--- app/views.py
+++ app/views.py
@@ -57,7 +57,7 @@ def register():
     # declare the Registration Form
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, admin_status=1)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
