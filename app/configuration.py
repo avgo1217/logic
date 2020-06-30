aa0110cb8ba7684cadb3e4f3e8aa9013abd002ed
Ryan Kim <1217rkim@gmail.com>
2020-06-29 21:13:16 -0500

    AWS Set up

--- app/configuration.py
+++ app/configuration.py
@@ -13,8 +13,8 @@ class Config():
 
 	CSRF_ENABLED = True
 	SECRET_KEY   = "77tgFCdrEEdv77554##@3" 
-	#SQLALCHEMY_DATABASE_URI = 'postgres://zabhqwgiinaxvr:1bcca0b0a8f92323d1020095f22980a1b0af1d027d98407716ce48ac711c95c4@ec2-54-211-210-149.compute-1.amazonaws.com:5432/d85mu1oa759pn4'
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RaliWeinskim@localhost/unlockd_test'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ryan.kim@n1esports.com:yN(<Zcu$Hnm?;9M@logic-database.cfhypkzsmt4g.us-east-2.rds.amazonaws.com/logic-database'
+	#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:RaliWeinskim@localhost/unlockd_test'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 465
@@ -26,6 +26,6 @@ class Config():
 	SESSION_TYPE = 'filesystem'
 	UPLOAD_FOLDER='app/uploads/'
 	ALLOWED_EXTENSIONS={'csv'}
-	#ROOT='https://warm-journey-64560.herokuapp.com/'
-	ROOT = 'http://127.0.0.1:5000/'
+	ROOT='http://0.0.0.0:5000'
+	#ROOT = 'http://127.0.0.1:5000/'
 	
