5546811e8bd38fa513d36f044bd4539354d35e1f
Ryan Kim <1217rkim@gmail.com>
2020-06-30 21:27:25 -0500

    aws prep

--- app/configuration.py
+++ app/configuration.py
@@ -26,6 +26,6 @@ class Config():
 	SESSION_TYPE = 'filesystem'
 	UPLOAD_FOLDER='app/uploads/'
 	ALLOWED_EXTENSIONS={'csv'}
-	ROOT='http://0.0.0.0:5000'
+	ROOT='http://127.0.0.0:5000/'
 	#ROOT = 'http://127.0.0.1:5000/'
 	
